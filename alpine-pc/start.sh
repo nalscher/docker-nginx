81e53a8d2b230300f2ad56bfb7d3c21705b09a72
Nicolas Alscher <nicolas.alscher@ihneo.fr>
2017-07-28 17:39:06 +0200

    PC project

---
+++ alpine-pc/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Replace variables in conf
+sed -i "s~ENV_APP_ROOT~${APP_ROOT}~" /etc/nginx/conf.d/default.conf
+sed -i "s/ENV_APP_ENVIRONMENT/${APP_ENVIRONMENT}/" /etc/nginx/conf.d/default.conf
+sed -i "s/ENV_APP_REGION/${APP_REGION}/" /etc/nginx/conf.d/default.conf
+
+# Start nginx
+exec nginx -g "daemon off;"
