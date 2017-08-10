d46a9e1463191cbbc5d35ce5e980817b57d7e654
Nicolas Alscher <nicolas.alscher@ihneo.fr>
2017-08-10 12:41:22 +0200

    Remove alpine-pc-dev

--- alpine-pc/start.sh
+++ alpine-pc/start.sh
@@ -6,5 +6,13 @@ sed -i "s/ENV_APP_ENVIRONMENT/${APP_ENVIRONMENT}/" /etc/nginx/conf.d/default.con
 sed -i "s/ENV_APP_DEPLOY_ENV/${APP_DEPLOY_ENV}/" /etc/nginx/conf.d/default.conf
 sed -i "s/ENV_APP_REGION/${APP_REGION}/" /etc/nginx/conf.d/default.conf
 
+# Change GUID et PUID
+if [ 100 -ne ${PUID} ] || [ 101 -ne ${GUID} ]
+then
+	usermod -u ${PUID} nginx
+	groupmod -g ${GUID} nginx
+	chown -R nginx:nginx /var/cache/nginx
+fi
+
 # Start nginx
 exec nginx -g "daemon off;"
